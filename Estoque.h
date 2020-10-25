f606168899d710acd9e1db340475ce48b57feee5
Bruno <bgomes.eng14@gmail.com>
2020-10-25 15:14:29 -0300

    adicionando o header de Estoque.c

---
+++ Estoque.h
@@ -0,0 +1,12 @@
+#ifndef ESTOQUE_H
+#define ESTOQUE_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+void imprimeMenu();
+void geracodigo(char *pais, int qtd, char *codigo);
+
+#endif
